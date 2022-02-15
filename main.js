46a58331914f8084f8c12d51be750a8cbc8b95b7
Petra Stejskalova <pt.stejskalova@gmail.com>
2022-02-15 21:10:52 +0100

    set onmouseover/out

--- main.js
+++ main.js
@@ -3,3 +3,25 @@
 let nadpis = document.querySelector('h1');
 nadpis.classList.add('zluty');
 nadpis.classList.remove('zluty');
+
+let pocitadlo = 0;
+function priNajetiNaCtverec() {
+    console.log('test ' + pocitadlo);
+    // pocitadlo = pocitadlo + 1;
+    pocitadlo++;
+}
+
+function setBold(element) {
+    element.style.fontWeight = 'bold';
+}
+
+function setNormal(element) {
+    element.style.fontWeight = 'normal';
+}
+
+function poKliknuti() {
+    let cervena = document.querySelector('.odstavec');
+    cervena.style.color = 'red';
+}
+
+
