2a8f1c3bf42aed161bfd76d7fb58bf8a2f327f9b
Petra Stejskalova <pt.stejskalova@gmail.com>
2022-02-18 10:53:14 +0100

    adding audio control

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function setNormal(element) {
 }
 
 let paragraph = document.querySelector('p');
+
 function addClass() {
     paragraph.classList.toggle('cerveny');
 }
@@ -30,4 +31,30 @@ function addClass() {
 function changeFontSize() {
     let computedSize = window.getComputedStyle(paragraph, null).getPropertyValue('font-size');
     paragraph.style.fontSize = (parseFloat(computedSize) + 1) + 'px';
+}
+
+let audioFile = document.getElementById('soundTrack');
+
+function startAudio() {
+    audioFile.play();
+}
+
+function pauseAudio() {
+    audioFile.pause();
+}
+
+function volumeDown() {
+    audioFile.volume = 0.0;
+}
+
+function volumeNormal() {
+    audioFile.volume = 0.5;
+}
+
+function volumeMax() {
+    audioFile.volume = 1.0;
+}
+
+function trackToBeginning() {
+    audioFile.currentTime = 0;
 }
